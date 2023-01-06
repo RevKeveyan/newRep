f71ff013dcb5268d71b99c4ce3646baf067d254c
Rev <rkeveyan23@gmail.com>
2023-01-06 20:59:24 +0400

    06.01.2023 20:59 add Hello World

---
+++ newrepos/src/App.js
@@ -0,0 +1,12 @@
+import Header1 from './homeWork/header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ newrepos/src/homeWork/header/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Header1 extends Component{
+
+
+    render (){
+        return <div>
+            <p>Hello World</p>
+        </div>
+    }
+}
+export default Header1;
